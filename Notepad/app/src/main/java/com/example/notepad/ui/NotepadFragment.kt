0e4bdaf4648e1c5198850eb0f2f3604a02a0e3a8
Jesse <sadsa@dsadas.nl>
2020-12-10 15:30:06 +0100

    4.1 add observer

--- Notepad/app/src/main/java/com/example/notepad/ui/NotepadFragment.kt
+++ Notepad/app/src/main/java/com/example/notepad/ui/NotepadFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 import com.example.notepad.R
+import com.example.notepad.viewmodel.NoteViewModel
+import kotlinx.android.synthetic.main.fragment_add_note.*
+import kotlinx.android.synthetic.main.fragment_notepad.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class NotepadFragment : Fragment() {
+
+    private val viewModel: NoteViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +31,19 @@ class NotepadFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        observeAddNoteResult()
+
     }
+
+    private fun observeAddNoteResult() {
+        viewModel.note.observe(viewLifecycleOwner, Observer{ note ->
+            note?.let {
+                tvNoteTitle.text = it.title
+                tvLastUpdated.text = getString(R.string.last_updated, it.lastUpdated.toString())
+                tvNoteText.text = it.text
+
+            }
+        })
+    }
+
 }
