95dac1103c584770e2089cdd6476e17f8779b798
Jesse <sadsa@dsadas.nl>
2020-12-10 15:39:27 +0100

    5.1 Check for errors/succes

--- Notepad/app/src/main/java/com/example/notepad/ui/AddNoteFragment.kt
+++ Notepad/app/src/main/java/com/example/notepad/ui/AddNoteFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -47,12 +48,22 @@ class AddNoteFragment : Fragment() {
             }
 
         })
+
+        //viemodel is not succesfully updated
+        viewModel.error.observe(viewLifecycleOwner, Observer { message ->
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+        })
+
+        //viewmodel is succesfully updated
+        viewModel.success.observe(viewLifecycleOwner, Observer {     success ->
+            //"pop" the backstack, this means we destroy this fragment and go back
+            findNavController().popBackStack()
+        })
     }
 
     private fun onSaveNote(){
         //update note through the repository in the database
         viewModel.updateNote(tilNoteTitle.editText?.text.toString(), tilNoteText.editText?.text.toString())
 
-        findNavController().popBackStack()
     }
 }
