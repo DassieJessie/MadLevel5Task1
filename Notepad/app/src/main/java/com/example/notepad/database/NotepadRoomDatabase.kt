d4698bdb06445f7284f705bcab2b0ab0d7799a10
Jesse <sadsa@dsadas.nl>
2020-12-10 15:06:37 +0100

    3.2 Add empty note
    when the database is created we want an empty note to be already showing

--- Notepad/app/src/main/java/com/example/notepad/database/NotepadRoomDatabase.kt
+++ Notepad/app/src/main/java/com/example/notepad/database/NotepadRoomDatabase.kt
@@ -5,9 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.notepad.converter.Converters
 import com.example.notepad.dao.NoteDao
 import com.example.notepad.model.Note
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
 //Using the @TypeConverters annotation the converters are added to the database.
@@ -32,6 +37,22 @@ abstract class NotepadRoomDatabase : RoomDatabase() {
                             NotepadRoomDatabase::class.java, DATABASE_NAME
                         )
                             .fallbackToDestructiveMigration()
+                            .addCallback(object : RoomDatabase.Callback() {
+                                //only gets called when the database is created
+                                override fun onCreate(db: SupportSQLiteDatabase) {
+                                    super.onCreate(db)
+                                    /**
+                                     * Using let we check if the INSTANCE is not null. If it’s not
+                                     * null then the note is added using a Coroutine and the noteDao
+                                     **/
+                                    INSTANCE?.let { database ->
+                                        CoroutineScope(Dispatchers.IO).launch {
+                                            database.noteDao().insertNote(Note("Title", Date(), ""))
+                                        }
+                                    }
+                                }
+                            })
+
                             .build()
                     }
                 }
