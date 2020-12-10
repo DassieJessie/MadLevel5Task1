0f8f2647fbd4766357af4a446fdc369236b52700
Jesse <sadsa@dsadas.nl>
2020-12-10 12:42:49 +0100

    Refactoring
    1. add packages
    2. change fragment 1 /2
    - remove buttons
    - change xml file
    3. add xml files
    4. change nav_graph

--- Notepad/app/src/main/java/com/example/notepad/ui/MainActivity.kt
+++ Notepad/app/src/main/java/com/example/notepad/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.notepad
+package com.example.notepad.ui
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.notepad.R
 
 class MainActivity : AppCompatActivity() {
 
